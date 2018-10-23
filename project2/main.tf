26fb3a8b0099542b42663ed66121e66f612e52fb
unai <unai.mayo@es.ibm.com>
2018-10-23 12:05:30 +0000

    fixed isue

--- project2/main.tf
+++ project2/main.tf
@@ -52,27 +52,26 @@ resource "vsphere_virtual_machine" "virtual_machine" {
   guest_id = "${data.vsphere_virtual_machine.virtual_machine_template.guest_id}"
   resource_pool_id = "${data.vsphere_compute_cluster.virtual_machine_compute_cluster.resource_pool_id}"
   
-  customize {
-    linux_options {
-      domain    = "redsys.lab"
-      host_name = "${var.virtual_machine_name}"
-    }
-
-    network_interface {
-      ipv4_address = "${var.vm_ipv4_address}"
-      ipv4_netmask = "24"
-    }
-
-    ipv4_gateway    = "10.129.254.1"
-    dns_suffix_list = "redsys.lab"
-    dns_server_list = "10.129.254.6"
-}
-  
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
   }
   clone {
     template_uuid = "${data.vsphere_virtual_machine.virtual_machine_template.id}"
+    customize {
+      linux_options {
+        domain    = "redsys.lab"
+        host_name = "${var.virtual_machine_name}"
+      }
+
+      network_interface {
+        ipv4_address = "${var.vm_ipv4_address}"
+        ipv4_netmask = "24"
+      }
+
+      ipv4_gateway    = "10.129.254.1"
+      dns_suffix_list = "redsys.lab"
+      dns_server_list = "10.129.254.6"
+    }
   }
   disk {
     label = "${var.virtual_machine_disk_name}"
