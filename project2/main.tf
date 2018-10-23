b12052bd9c0acf7639dec88158d20419a05e4103
unai <unai.mayo@es.ibm.com>
2018-10-23 09:27:27 +0000

    remove connection parametes from vsphere provider

--- project2/main.tf
+++ project2/main.tf
@@ -9,10 +9,6 @@ terraform {
 }
 
 provider "vsphere" {
-  user           = "${var.user}"
-  password       = "${var.password}"
-  vsphere_server = "${var.vsphere_server}"
-
   allow_unverified_ssl = "${var.allow_unverified_ssl}"
   version = "~> 1.2"
 }
